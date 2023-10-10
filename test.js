77b05b4ee8162c57127620f9e88805473ad3cd51
Tarique12344 <Tariqueelshamy11@gmail.com>
2023-10-10 17:38:44 -0400

    FunctionHw

---
+++ test.js
@@ -0,0 +1,21 @@
+var a = 10;
+var b = 5;
+
+function sum(a, b){
+    return a + b;
+}
+console.log(sum(a, b));
+
+function minus(a, b){
+    return a - b;
+}
+console.log(minus(a, b));
+
+function divide(a, b){
+    return a / b;
+}
+console.log(divide(a, b));
+function multiple(a, b){
+    return a *  b;
+}
+console.log(multiple(a, b));
